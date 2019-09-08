26684c1ba007115b9ccc55b25d0365f4bcabc144
mirror0901 <1755496180@qq.com>
2019-09-09 00:39:39 +0800

    #功能完善

--- vuehr-demo/src/components/salary/SalSob.vue
+++ vuehr-demo/src/components/salary/SalSob.vue
@@ -1,3 +1,42 @@
 <template>
   <div></div>
 </template>
+<script>
+  export default {
+    data () {
+      return {
+        dialogVisible: false,
+        tableLoading: false,
+        index: 0,
+        salaries: [],
+        multipleSelection: [],
+        salary:
+          {
+            id: '',
+            createDate:
+              '',
+            basicSalary:
+              '',
+            trafficSalary:
+              '',
+            lunchSalary:
+              '',
+            bonus:
+              '',
+            pensionBase:
+              '',
+            pensionPer:
+              '',
+            medicalBase:
+              '',
+            medicalPer:
+              '',
+            accumulationFundBase:
+              '',
+            accumulationFundPer:
+              ''
+          }
+      }
+    }
+  }
+</script>
